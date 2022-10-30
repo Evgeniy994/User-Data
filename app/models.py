dd02a320e13e307d2a15c8385f6ae96653597b50
Evgeniy Tretyak <Evgeniy@iMac-Yana.local>
2022-10-30 22:20:32 +0200

    UserData v1

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class UserData(AbstractUser):
+    first_name = models.CharField(max_length=49)
+    last_name = models.CharField(max_length=99)
+
+    class Meta:
+        ordering = ["first_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
